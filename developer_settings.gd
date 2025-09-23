7983b326db173edaa01f13bb1363d34ed7def981
Allen Pestaluky <allenpestaluky@gmail.com>
2025-09-23 11:57:08 -0400

    Moved reference monitor calibration to be separate from player-facing in-game settings

--- developer_settings.gd
+++ developer_settings.gd
@@ -4,6 +4,43 @@ func _process(_delta: float) -> void:
 	var high_precision: bool = get_window().hdr_output_prefer_high_precision
 	if %PreferHighPrecision.button_pressed != high_precision:
 		%PreferHighPrecision.button_pressed = high_precision
+	
+	var window: Window = get_window()
+	var adjustment_step_1 = 0.2
+	var adjustment_step_2 = 0.4
+	
+	var max_lum: float = window.hdr_output_max_luminance / window.hdr_output_reference_luminance
+	var sm: ShaderMaterial = %MaxLumAdjustBacking.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_lum, max_lum, max_lum))
+	
+	var max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) - adjustment_step_2)
+	sm = %MaxLumAdjustBelow2.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
+
+	max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) - adjustment_step_1)
+	sm = %MaxLumAdjustBelow3.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
+	
+	max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) + adjustment_step_2)
+	sm = %MaxLumAdjustAbove2.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
+	
+	max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) + adjustment_step_1)
+	sm = %MaxLumAdjustAbove3.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
+	
+	#Examples:
+	
+	sm = %ExampleMaxLumAdjustBacking.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3.ONE)
+	
+	max_adjust_material_value = pow(2.0, (log(1.0) / log(2.0)) - adjustment_step_2)
+	sm = %ExampleMaxLumAdjustBelow.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
+
+	max_adjust_material_value = pow(2.0, (log(1.0) / log(2.0)) - adjustment_step_1)
+	sm = %ExampleMaxLumAdjustBelow2.material as ShaderMaterial
+	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
 
 
 func _on_prefer_high_precision_toggled(toggled_on: bool) -> void:
--- in_game_hdr_settings.gd
+++ in_game_hdr_settings.gd
@@ -64,42 +64,6 @@ func _process(_delta: float) -> void:
 	
 	%ResetBrightness.disabled = window.hdr_output_auto_adjust_reference_luminance
 	%ResetMaxLum.disabled = window.hdr_output_auto_adjust_max_luminance
-	
-	var adjustment_step_1 = 0.2
-	var adjustment_step_2 = 0.4
-	
-	var max_lum: float = window.hdr_output_max_luminance / window.hdr_output_reference_luminance
-	var sm: ShaderMaterial = %MaxLumAdjustBacking.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_lum, max_lum, max_lum))
-	
-	var max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) - adjustment_step_2)
-	sm = %MaxLumAdjustBelow2.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
-
-	max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) - adjustment_step_1)
-	sm = %MaxLumAdjustBelow3.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
-	
-	max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) + adjustment_step_2)
-	sm = %MaxLumAdjustAbove2.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
-	
-	max_adjust_material_value = pow(2.0, (log(max_lum) / log(2.0)) + adjustment_step_1)
-	sm = %MaxLumAdjustAbove3.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
-	
-	#Examples:
-	
-	sm = %ExampleMaxLumAdjustBacking.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3.ONE)
-	
-	max_adjust_material_value = pow(2.0, (log(1.0) / log(2.0)) - adjustment_step_2)
-	sm = %ExampleMaxLumAdjustBelow.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
-
-	max_adjust_material_value = pow(2.0, (log(1.0) / log(2.0)) - adjustment_step_1)
-	sm = %ExampleMaxLumAdjustBelow2.material as ShaderMaterial
-	sm.set_shader_parameter("colour", Vector3(max_adjust_material_value, max_adjust_material_value, max_adjust_material_value))
 
 
 func _on_hdr_check_button_toggled(toggled_on: bool) -> void:

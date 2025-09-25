3703d7f5643d335bb95cb438bce7424d87c6920b
Allen Pestaluky <allenpestaluky@gmail.com>
2025-09-25 14:02:50 -0400

    hacked up to work without hdr for a colour adjustments test

--- colour_sweep.gd
+++ colour_sweep.gd
@@ -7,10 +7,10 @@ func _process(_delta: float) -> void:
 	var window: Window = get_window()
 	var sm: ShaderMaterial = %ColourSweepMesh.material as ShaderMaterial
 	sm.set_shader_parameter("clip_to_max", clip_to_max_lum)
-	sm.set_shader_parameter("max_value", window.get_hdr_output_max_value());
-	sm.set_shader_parameter("clip_in_bt2020", window.hdr_output_enabled)
-	%SweepMinLabel.text = "%+0.2f (linear %0.5f, %0.0f nits)" % [%MinHSlider.value, pow(2, %MinHSlider.value), pow(2, %MinHSlider.value) * get_window().hdr_output_reference_luminance]
-	%SweepMaxLabel.text = "%+0.2f (linear %0.5f, %0.0f nits)" % [%MaxHSlider.value, pow(2, %MaxHSlider.value), pow(2, %MaxHSlider.value) * get_window().hdr_output_reference_luminance]
+	#sm.set_shader_parameter("max_value", window.get_hdr_output_max_value());
+	#sm.set_shader_parameter("clip_in_bt2020", window.hdr_output_enabled)
+	#%SweepMinLabel.text = "%+0.2f (linear %0.5f, %0.0f nits)" % [%MinHSlider.value, pow(2, %MinHSlider.value), pow(2, %MinHSlider.value) * get_window().hdr_output_reference_luminance]
+	#%SweepMaxLabel.text = "%+0.2f (linear %0.5f, %0.0f nits)" % [%MaxHSlider.value, pow(2, %MaxHSlider.value), pow(2, %MaxHSlider.value) * get_window().hdr_output_reference_luminance]
 
 
 func _on_min_h_slider_value_changed(value: float) -> void:

3db671a46a297eb3c7231edacf60940035517c21
Allen Pestaluky <allenpestaluky@gmail.com>
2025-11-27 10:19:21 -0500

    Trying out a new paradigm of "normalizing" Color to the max value. I think this is the sort of helper function people will want.

--- dynamic_colour_values/max_colour.gd
+++ dynamic_colour_values/max_colour.gd
@@ -5,15 +5,19 @@ extends ColorRect
 
 
 func _process(_delta: float) -> void:
-	var bright_colour = sdr_colour
+	# Adjust the brightness of color to be the brightest possible, regardless
+	# of SDR or HDR output.
+	color = normalize_color(sdr_colour, get_window().get_output_max_linear_value())
+
+
+func normalize_color(srgb_color: Color, max_linear_value: float = 1.0) -> Color:
 	# Color must be linear-encoded to use math operations correctly.
-	bright_colour = bright_colour.srgb_to_linear()
-	# Record the original alpha value.
-	var original_alpha = bright_colour.a
-	# Adjust brightness to be the brightest possible, regardless of SDR or HDR output.
-	bright_colour = bright_colour * get_window().get_output_max_linear_value()
-	# Restore the original alpha channel value.
-	bright_colour.a = original_alpha
-	# Convert back to nonlinear sRGB encoding.
-	bright_colour = bright_colour.linear_to_srgb()
-	color = bright_colour
+	var linear_color: Color = srgb_color.srgb_to_linear()
+	var max_color_channel: float = maxf(linear_color.r, maxf(linear_color.g, linear_color.b))
+	var brightness_scale: float = max_linear_value / max_color_channel
+	var normalized_color: Color = linear_color  * brightness_scale
+	# The alpha channel value should not be affected by normalization.
+	normalized_color.a = linear_color.a
+	# Convert back to nonlinear sRGB encoding, which is required for Color in
+	# Godot unless stated otherwise.
+	return normalized_color.linear_to_srgb()

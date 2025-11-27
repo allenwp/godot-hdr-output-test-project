a235a2f48d13191c0946c5a308ea59eefbf27adb
Allen Pestaluky <allenpestaluky@gmail.com>
2025-11-27 17:14:06 -0500

    Made more modifications to the normalize_color function that I plan to add to the docs for get_output_max_linear_value()

--- dynamic_colour_values/max_colour.gd
+++ dynamic_colour_values/max_colour.gd
@@ -10,14 +10,14 @@ func _process(_delta: float) -> void:
 	color = normalize_color(sdr_colour, get_window().get_output_max_linear_value())
 
 
-func normalize_color(srgb_color: Color, max_linear_value: float = 1.0) -> Color:
-	# Color must be linear-encoded to use math operations correctly.
-	var linear_color: Color = srgb_color.srgb_to_linear()
-	var max_color_channel: float = maxf(linear_color.r, maxf(linear_color.g, linear_color.b))
-	var brightness_scale: float = max_linear_value / max_color_channel
-	var normalized_color: Color = linear_color  * brightness_scale
-	# The alpha channel value should not be affected by normalization.
-	normalized_color.a = linear_color.a
+func normalize_color(srgb_color, max_linear_value = 1.0):
+	# Color must be linear-encoded to use math operations.
+	var linear_color = srgb_color.srgb_to_linear()
+	var max_rgb_value = maxf(linear_color.r, maxf(linear_color.g, linear_color.b))
+	var brightness_scale = max_linear_value / max_rgb_value
+	linear_color *= brightness_scale
+	# Undo changes to the alpha channel, which should not be modified.
+	linear_color.a = srgb_color.a
 	# Convert back to nonlinear sRGB encoding, which is required for Color in
 	# Godot unless stated otherwise.
-	return normalized_color.linear_to_srgb()
+	return linear_color.linear_to_srgb()

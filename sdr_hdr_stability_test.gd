4b68ac3597cf757e93be0628d944117ecfd79835
Allen Pestaluky <allenpestaluky@gmail.com>
2025-12-08 16:37:57 -0500

    temp test project for steam deck luminance matching

--- sdr_hdr_stability_test.gd
+++ sdr_hdr_stability_test.gd
@@ -3,19 +3,22 @@ extends Node2D
 
 func _ready() -> void:
 	var window_id = get_window().get_window_id()
-	DisplayServer.window_set_hdr_output_reference_luminance(-1, window_id)
-	DisplayServer.window_set_hdr_output_max_luminance(-1, window_id)
+	DisplayServer.window_set_hdr_output_reference_luminance(180, window_id)
+	DisplayServer.window_set_hdr_output_max_luminance(250, window_id)
 
 
 func _process(_delta: float) -> void:
-	var window_id = get_window().get_window_id()
+	var window = get_window()
+	var window_id = window.get_window_id()
 	var hdr_output_enabled: bool = DisplayServer.window_is_hdr_output_enabled(window_id)
 	var ref_luminance = DisplayServer.window_get_hdr_output_current_reference_luminance()
 	var max_luminance = DisplayServer.window_get_hdr_output_current_max_luminance()
 	if hdr_output_enabled:
-		%StabilityTestText.text = "HDR\nReference white luminance: %s\nMaximum luminance: %s" % [ref_luminance, max_luminance]
+		%StabilityTestText.text = "HDR\nReference white luminance: %s" % [ref_luminance]
 	else:
 		%StabilityTestText.text = "SDR"
+		if window.hdr_output_requested:
+			%StabilityTestText.text += " (HDR requested)"
 
 
 func _on_toggle_hdr_timer_timeout() -> void:

08008a3ede34c965525d8ebe9868a9bbdfc041a7
Allen Pestaluky <allenpestaluky@gmail.com>
2025-12-15 16:47:51 -0500

    Reorganized files and swapped meshes for colour rects where possible.

--- dynamic_colour_values/max.gd
+++
@@ -1,8 +0,0 @@
-extends MeshInstance2D
-
-
-func _process(_delta: float) -> void:
-	var window: Window = get_window()
-	var sm = material as ShaderMaterial
-	var max_val: float = window.get_output_max_linear_value()
-	sm.set_shader_parameter("colour", Vector3(max_val, max_val, max_val))
